2cbbfbfb4f457b09b2456d618b4b9cb490929e24
John Doe <johndoe@example.com>
2024-08-20 01:38:29 +0800

    Authentication: none、hostbased

--- src/Session.swift
+++ src/Session.swift
@@ -144,7 +144,14 @@ public extension SSH {
     /// - Parameter password: 要用于身份验证的密码字符串。
     /// - Returns: 如果身份验证成功则返回true，否则返回false。
     func authenticate(password: String) async -> Bool {
-        await call {
+        guard let rawSession = rawSession else {
+            return false
+        }
+        let list = await userauth()
+        guard list.contains("password") else {
+            return false
+        }
+        return await call {
             guard let rawSession = self.rawSession else {
                 return false
             }
@@ -166,7 +173,14 @@ public extension SSH {
     ///   - publickeyFile: 可选的公钥文件路径，默认为空字符串。
     /// - Returns: 如果身份验证成功则返回true，否则返回false。
     func authenticate(privateKeyFile: String, passphrase: String? = nil, publickeyFile: String = "") async -> Bool {
-        await call {
+        guard let rawSession = rawSession else {
+            return false
+        }
+        let list = await userauth()
+        guard list.contains("publickey") else {
+            return false
+        }
+        return await call {
             guard let rawSession = self.rawSession else {
                 return false
             }
@@ -189,12 +203,42 @@ public extension SSH {
     ///   - publickey: 可选的公钥字符串，默认为空字符串。
     /// - Returns: 如果身份验证成功则返回true，否则返回false。
     func authenticate(privateKey: String, passphrase: String? = nil, publickey: String = "") async -> Bool {
-        await call {
-            guard let rawSession = self.rawSession else {
-                return false
-            }
+        guard let rawSession = rawSession else {
+            return false
+        }
+        let list = await userauth()
+        guard list.contains("publickey") else {
+            return false
+        }
+        return await call {
             let code = self.callSSH2 {
                 libssh2_userauth_publickey_frommemory(rawSession, self.user, self.user.count, publickey, publickey.count, privateKey, privateKey.count, passphrase)
+            }
+            guard code == LIBSSH2_ERROR_NONE, self.isAuthenticated else {
+                return false
+            }
+            return true
+        }
+    }
+
+    /// 使用主机密钥认证方法对SSH会话进行身份验证。
+    /// - Parameters:
+    ///   - hostname: 远程主机名或IP地址。
+    ///   - privateKeyFile: 私钥文件路径。
+    ///   - passphrase: 私钥的密码（可选）。
+    ///   - publickeyFile: 公钥文件路径（默认为空字符串，表示使用私钥文件中的公钥）。
+    /// - Returns: 如果身份验证成功返回true，否则返回false。
+    func authenticate(hostname: String, privateKeyFile: String, passphrase: String? = nil, publickeyFile: String = "") async -> Bool {
+        guard let rawSession = rawSession else {
+            return false
+        }
+        let list = await userauth()
+        guard list.contains("hostbased") else {
+            return false
+        }
+        return await call {
+            let code = self.callSSH2 {
+                libssh2_userauth_hostbased_fromfile_ex(rawSession, self.user, self.user.countUInt32, publickeyFile, privateKeyFile, passphrase, hostname, hostname.countUInt32, self.user, self.user.countUInt32)
             }
             guard code == LIBSSH2_ERROR_NONE, self.isAuthenticated else {
                 return false
@@ -206,36 +250,50 @@ public extension SSH {
     // 使用键盘交互进行身份验证
     /// 通过键盘交互方式进行SSH会话身份验证。
     /// 这种方法通常用于需要响应提示的情况，例如输入密码或PIN码。
-    /// - Returns: 如果身份验证成功则返回true，否则返回false。
-    func authenticate() async -> Bool {
-        await call {
-            guard let rawSession = self.rawSession else {
+    /// - Parameter none: 如果为true，则不使用无验证登录；如果为false，则使用键盘交互方式进行认证。
+    /// - Returns: 返回一个布尔值，表示认证是否成功。
+    func authenticate(_ none: Bool = false) async -> Bool {
+        guard let rawSession = rawSession else {
+            return false
+        }
+        if none {
+            let list = await userauth()
+            guard list.contains("none") else {
                 return false
             }
-            let code = self.callSSH2 {
-                libssh2_userauth_keyboard_interactive_ex(rawSession, self.user, self.user.countUInt32) { _, _, _, _, numPrompts, prompts, responses, abstract in
-                    let ssh = SSH.getSSH(from: abstract!)
-                    for i in 0 ..< Int(numPrompts) {
-                        guard let promptI = prompts?[i], let text = promptI.text else {
-                            continue
+            return isAuthenticated
+        } else {
+            let list = await userauth()
+            guard list.contains("keyboard-interactive") else {
+                return false
+            }
+            return await call {
+                let code = self.callSSH2 {
+                    libssh2_userauth_keyboard_interactive_ex(rawSession, self.user, self.user.countUInt32) { _, _, _, _, numPrompts, prompts, responses, abstract in
+                        let ssh = SSH.getSSH(from: abstract!)
+                        for i in 0 ..< Int(numPrompts) {
+                            guard let promptI = prompts?[i], let text = promptI.text else {
+                                continue
+                            }
+
+                            let data = Data(bytes: UnsafeRawPointer(text), count: Int(promptI.length))
+
+                            guard let challenge = String(data: data, encoding: .utf8) else {
+                                continue
+                            }
+
+                            let password = ssh.sessionDelegate?.keyboardInteractive(ssh: ssh, prompt: challenge) ?? ""
+                            let response = LIBSSH2_USERAUTH_KBDINT_RESPONSE(text: password.pointerCChar, length: password.countUInt32)
+                            responses?[i] = response
                         }
-
-                        let data = Data(bytes: UnsafeRawPointer(text), count: Int(promptI.length))
-
-                        guard let challenge = String(data: data, encoding: .utf8) else {
-                            continue
-                        }
-
-                        let password = ssh.sessionDelegate?.keyboardInteractive(ssh: ssh, prompt: challenge) ?? ""
-                        let response = LIBSSH2_USERAUTH_KBDINT_RESPONSE(text: password.pointerCChar, length: password.countUInt32)
-                        responses?[i] = response
                     }
                 }
+                guard code == LIBSSH2_ERROR_NONE else {
+                    return false
+                }
+
+                return self.isAuthenticated
             }
-            guard code == LIBSSH2_ERROR_NONE, self.isAuthenticated else {
-                return false
-            }
-            return true
         }
     }
 
@@ -271,21 +329,21 @@ public extension SSH {
         }
     }
 
-    // 用户认证列表
-    /// 返回当前会话支持的用户认证方法列表。
-    /// - Returns: 支持的认证方法列表字符串，如果出错则返回nil。
-    func userauthList() async -> String? {
+    /// 用户认证列表
+    /// 该函数异步获取当前会话支持的用户认证方法列表。
+    /// - Returns: 返回一个字符串数组，包含所有支持的用户认证方法。
+    func userauth() async -> [String] {
         await call {
             guard let rawSession = self.rawSession else {
-                return nil
+                return []
             }
             let ptr = self.callSSH2 {
                 libssh2_userauth_list(rawSession, self.user, UInt32(self.user.count))
             }
             guard let ptr else {
-                return nil
+                return []
             }
-            return ptr.string
+            return ptr.string.components(separatedBy: ",")
         }
     }
 
