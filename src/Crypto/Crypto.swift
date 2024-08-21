1c1738897e601b84b03cb15198a8ed7667436c46
sshterm <admin@ssh2.app>
2024-08-21 13:21:22 +0800

    Crypto version

--- src/Crypto/Crypto.swift
+++ src/Crypto/Crypto.swift
@@ -3,7 +3,23 @@
 // Created by admin@ssh2.app 2024/8/18.
 
 import Foundation
-
+#if OPEN_SSL
+    import OpenSSL
+#else
+    import wolfSSL
+#endif
 public class Crypto {
     public static let shared: Crypto = .init()
+
+    #if OPEN_SSL
+        public let name = "OpenSSL"
+    #else
+        public let name = "wolfSSL"
+    #endif
+
+    #if OPEN_SSL
+        public let version = OPENSSL_VERSION_STR
+    #else
+        public let version = LIBWOLFSSL_VERSION_STRING
+    #endif
 }
