dea41f05ac2791fecd47be183c6ed3ec1064fbe7
sshterm <admin@ssh2.app>
2024-08-21 08:34:11 +0800

    algorithm

--- src/Crypto/Algorithm.swift
+++ src/Crypto/Algorithm.swift
@@ -6,7 +6,7 @@ import Foundation
 import wolfSSL
 
 public enum Algorithm: String, CaseIterable {
-    case md5, sha1, sha224, sha256, sha384, sha512
+    case md5, sha1, sha224, sha256, sha384, sha512, sha512_224, sha512_256, sha3_224, sha3_256, sha3_384, sha3_512, ripemd160
 
     // 返回对应算法的OpaquePointer，用于后续的加密操作
     public var EVP: UnsafePointer<WOLFSSL_EVP_MD>? {
@@ -23,6 +23,20 @@ public enum Algorithm: String, CaseIterable {
             wolfSSL_EVP_sha384()
         case .sha512:
             wolfSSL_EVP_sha512()
+        case .sha512_224:
+            wolfSSL_EVP_sha512_224()
+        case .sha512_256:
+            wolfSSL_EVP_sha512_256()
+        case .sha3_224:
+            wolfSSL_EVP_sha3_224()
+        case .sha3_256:
+            wolfSSL_EVP_sha3_256()
+        case .sha3_384:
+            wolfSSL_EVP_sha3_384()
+        case .sha3_512:
+            wolfSSL_EVP_sha3_512()
+        case .ripemd160:
+            wolfSSL_EVP_ripemd160()
         }
     }
 
@@ -46,6 +60,20 @@ public enum Algorithm: String, CaseIterable {
             48
         case .sha512:
             64
+        case .sha512_224:
+            28
+        case .sha512_256:
+            32
+        case .sha3_224:
+            28
+        case .sha3_256:
+            32
+        case .sha3_384:
+            48
+        case .sha3_512:
+            64
+        case .ripemd160:
+            20
         }
     }
 }
